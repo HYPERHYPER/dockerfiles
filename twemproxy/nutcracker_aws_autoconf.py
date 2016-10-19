7255526dca5e1afceccb5ef32c54838cb72f49ee
Kevin Cox <kevintcox@gmail.com>
2016-10-19 17:11:10 -0400

    Development version of twemproxy builds

--- twemproxy/nutcracker_aws_autoconf.py
+++ twemproxy/nutcracker_aws_autoconf.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python
 
 import boto3
+import botocore.exceptions
+import sys
 import yaml
+
 from pprint import pprint
 
 # configuration
 CLUSTER_TAG_KEY = "PhhhotoTwemproxyMember"
 CLUSTER_TAG_VALUE = "true"
 
-TWEMPROXY_CONFIG_INPUT_FILE = 'nutcracker.cfg'
-TWEMPROXY_CONFIG_OUTPUT_FILE = 'nutcracker.out.cfg'
+# This depends on some things in the environment. It will use credentials
+# in ~/.aws/config (useful in development). It can also just use AWS_DEFAULT_REGION
+# and whatever permissions the container / EC2 instance has via IAM.
+#
+# for example:
+# export AWS_DEFAULT_REGION=us-east-1
 
 # "static memory"
 _AWS_ACCT_ID = None
@@ -43,21 +50,54 @@ def get_twemproxy_address(node):
   """Get a twemproxy-formatted address string (uses weight '1')"""
   return "%s:%s:1" % (node['Endpoint']['Address'], node['Endpoint']['Port'])
 
+def get_session_region_client():
+  """
+  Magic Alert! Tries to make an elasticache session. If it fails
+  because no AWS region is set in the environment, it hits
+  http://169.254.169.254/latest/meta-data/placement/availability-zone
+  to try to get the current one. If it can't, we crash.
+  """
+  session = boto3.session.Session()
+  try:
+    client = session.client('elasticache')
+  except botocore.exceptions.NoRegionError, e:
+    # no region, so ask this mysterious AWS URL
+    print "no AWS region; get one from http://169.254.169.254/latest/meta-data/placement/availability-zone..."
+    import requests
+    import os
+    resp = requests.get('http://169.254.169.254/latest/meta-data/placement/availability-zone')
+    region = resp.text[:-1] # trim the last char (us-east-1b -> us-east-1)
+    print "found region %s" % region
+    os.environ['AWS_DEFAULT_REGION'] = region
+
+    # try it again, if this doesn't work, just crash
+    client = session.client('elasticache')
+
+  region = session.region_name
+
+  print "got", session, region, client
+  return session, region, client
+
 def get_engine_addresses():
-  """Hits the AWS API and returns a structure like:
+  """
+  Hits the AWS API and returns a structure like:
   {'memcached': ['cac-me-1tvncei2gal0r.ihepav.0001.use1.cache.amazonaws.com:11211:1'],
    'redis': []}
   """
-  session = boto3.session.Session()
-  region = session.region_name
-  client = session.client('elasticache')
+  session, region, client = get_session_region_client()
 
   engine_addresses = {
     'redis': [],
     'memcached': []
   }
 
-  clusters = get_clusters(client)
+  try:
+    clusters = get_clusters(client)
+  except botocore.exceptions.NoCredentialsError, e:
+    print "No AWS credentials found. this is expected to happen during `docker build`."
+    print "We'll skip the AWS API part and use the input config file as is."
+    return None
+
   print "Found %d ElastiCache clusters..." % len(clusters)
 
   for i, cluster in enumerate(clusters):
@@ -85,23 +125,37 @@ def get_engine_addresses():
   pprint(engine_addresses)
   return engine_addresses
 
-def update_twemproxy_config(addresses):
-  global TWEMPROXY_CONFIG_INPUT_FILE, TWEMPROXY_CONFIG_OUTPUT_FILE
-
+def update_twemproxy_config(addresses, input_filename, output_filename):
   # load config
-  f = open(TWEMPROXY_CONFIG_INPUT_FILE, 'r')
+  f = open(input_filename, 'r')
   config = yaml.load(f)
   f.close()
 
-  for engine in ['memcached', 'redis']:
-    config[engine]['servers'] = addresses[engine]
+  # update config
+  if addresses is not None:
+    # addresses can be None when the AWS API is not reachable; just write input to output
+    for engine in ['memcached', 'redis']:
+      config[engine]['servers'] = addresses[engine]
+  new_config = yaml.dump(config)
 
-  f = open(TWEMPROXY_CONFIG_OUTPUT_FILE, 'w')
-  f.write(yaml.dump(config))
+  # write new config
+  f = open(output_filename, 'w')
+  f.write(new_config)
   f.close()
 
-  print "wrote %s" % TWEMPROXY_CONFIG_OUTPUT_FILE
+  print "wrote the following to %s:" % output_filename
+  print new_config
+
 
 if __name__ == '__main__':
+  if len(sys.argv) != 3:
+    print "Usage: python nutcracker_aws_autoconf.py inputfilename outputfilename"
+    raise SystemExit
+
+  input_filename = sys.argv[1]
+  output_filename = sys.argv[2]
+
+  print "Reading %s, writing %s" % (input_filename, output_filename)
+
   addresses = get_engine_addresses()
-  update_twemproxy_config(addresses)
+  update_twemproxy_config(addresses, input_filename, output_filename)
